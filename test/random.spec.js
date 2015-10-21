60d52c95c4b45434105a7fed01e14e52fc55dd28
Erik Jung <erikdavidjung@gmail.com>
2015-10-20 20:00:45 -0700

    Add another assertion to random helper test

--- test/random.spec.js
+++ test/random.spec.js
@@ -8,10 +8,18 @@ Handlebars.registerHelper(random.name, random);
 
 tape('random', function (test) {
   var template = Handlebars.compile('{{random items}}');
-  var items = ['a', 'b', 'c'];
-  var result = template({ items: items });
-  var inArray = items.indexOf(result) !== -1;
+  var items;
+  var result;
 
-  test.plan(1);
-  test.ok(inArray, 'Works');
+  test.plan(2);
+
+  items = ['a', 'b', 'c'];
+  result = template({ items: items });
+  test.ok(items.indexOf(result) !== -1, 'Works');
+
+  try {
+    result = template({ items: 'not an array' });
+  } catch (err) {
+    test.pass('Errors when passed a non-array');
+  }
 });
