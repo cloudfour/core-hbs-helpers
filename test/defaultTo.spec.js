ea6cf31ec7cd8bfc8d91cae6186583e681e1c4ec
Erik Jung <erikdavidjung@gmail.com>
2015-11-13 16:35:14 -0800

    Add defaultTo helper

---
+++ test/defaultTo.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var defaultTo = require('../').defaultTo;
+var tape = require('tape');
+var Handlebars = require('handlebars');
+
+Handlebars.registerHelper(defaultTo.name, defaultTo);
+
+tape('defaultTo', function (test) {
+  var template;
+  var expected;
+  var actual;
+
+  test.plan(3);
+
+  template = Handlebars.compile('{{defaultTo doesExist "Goodbye"}}');
+  expected = 'Hello';
+  actual = template({ doesExist: 'Hello' });
+  test.equal(actual, expected, 'Works with value set');
+
+  template = Handlebars.compile('{{defaultTo doesNotExist "Goodbye"}}');
+  expected = 'Goodbye';
+  actual = template({});
+  test.equal(actual, expected, 'Works with value not set');
+
+  template = Handlebars.compile('{{defaultTo doesNotExist}}');
+  expected = '';
+  actual = template({});
+  test.equal(actual, expected, 'Works with value and fallback not set');
+});
