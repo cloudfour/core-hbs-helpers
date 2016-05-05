8439e286536e7b2225cb0600d196ba4f8b733f75
Tyler Sticka <tyler@tylersticka.com>
2016-05-05 16:28:14 -0700

    Allow random helper to accept multiple args
    Sometimes it's helpful when prototyping to be able to pass along multiple arguments to choose from at random instead of an array.

--- test/random.spec.js
+++ test/random.spec.js
@@ -7,21 +7,30 @@ var Handlebars = require('handlebars');
 Handlebars.registerHelper(random.name, random);
 
 tape('random', function (test) {
-  var template = Handlebars.compile('{{random items}}');
-  var items;
+  var items = ['a', 'b', 'c'];
+  var template;
   var result;
 
-  test.plan(2);
+  test.plan(4);
 
-  items = ['a', 'b', 'c'];
+  template = Handlebars.compile('{{random items}}');
   result = template({ items: items });
-  test.ok(items.indexOf(result) !== -1, 'Works');
+  test.ok(items.indexOf(result) !== -1, 'Works with a single Array');
 
+  template = Handlebars.compile('{{random "' + items.join('" "') + '"}}');
+  result = template();
+  test.ok(items.indexOf(result) !== -1, 'Works with multiple arguments');
+
+  template = Handlebars.compile('{{random "a"}}');
+  result = template();
+  test.equal(result, 'a', 'Works with only a single item');
+
+  template = Handlebars.compile('{{random}}');
   test.throws(
     function () {
-      template({ items: 'not an array' })
+      template();
     },
-    /passed an Array\.$/,
-    'Errors when passed a non-array'
+    /at least one argument\.$/,
+    'Errors when passed zero arguments'
   );
 });
