14a7cb77e60ce94aa3cf8a2aa0e3cfda6b8ceafc
Tyler Sticka <tyler@tylersticka.com>
2016-05-06 08:46:01 -0700

    random → randomItem
    Renaming this helper to be more specific. Also makes way for future random content generator helper.
    
    See: #27

--- test/randomItem.spec.js
+++ test/randomItem.spec.js
@@ -1,31 +1,31 @@
 'use strict';
 
-var random = require('../').random;
+var randomItem = require('../').randomItem;
 var tape = require('tape');
 var Handlebars = require('handlebars');
 
-Handlebars.registerHelper(random.name, random);
+Handlebars.registerHelper(randomItem.name, randomItem);
 
-tape('random', function (test) {
+tape('randomItem', function (test) {
   var items = ['a', 'b', 'c'];
   var template;
   var result;
 
   test.plan(4);
 
-  template = Handlebars.compile('{{random items}}');
+  template = Handlebars.compile('{{randomItem items}}');
   result = template({ items: items });
   test.ok(items.indexOf(result) !== -1, 'Works with a single Array');
 
-  template = Handlebars.compile('{{random "' + items.join('" "') + '"}}');
+  template = Handlebars.compile('{{randomItem "' + items.join('" "') + '"}}');
   result = template();
   test.ok(items.indexOf(result) !== -1, 'Works with multiple arguments');
 
-  template = Handlebars.compile('{{random "a"}}');
+  template = Handlebars.compile('{{randomItem "a"}}');
   result = template();
   test.equal(result, 'a', 'Works with only a single item');
 
-  template = Handlebars.compile('{{random}}');
+  template = Handlebars.compile('{{randomItem}}');
   test.throws(
     function () {
       template();
