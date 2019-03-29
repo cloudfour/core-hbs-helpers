4654c6e4b720ec3583f8645b5ba77022b0bff8e4
Paul Hebert <paul@cloudfour.com>
2019-03-29 16:20:18 -0700

    Added a helperfor string replacement

---
+++ test/replaceAll.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var replaceAll = require('../').replaceAll;
+var tape = require('tape');
+var Handlebars = require('handlebars');
+
+Handlebars.registerHelper(replaceAll.name, replaceAll);
+
+tape('replaceAll', function (test) {
+  var template;
+  var actual;
+  var expected;
+
+  test.plan(4);
+
+  expected = '9';
+  actual = Handlebars.compile('{{replaceAll "9:00" ":00" ""}}');
+  test.equal(actual(), expected, 'Replaces a single instance of a string with an empty string');
+
+  expected = 'xcllnt';
+  actual = Handlebars.compile('{{replaceAll "excellent" "e" ""}}');
+  test.equal(actual(), expected, 'Replaces multiple instances of a character with an empty string');
+
+  expected = 'She sells sea barbells by the seabarbore';
+  actual = Handlebars.compile('{{replaceAll "She sells sea shells by the seashore" "sh" "barb"}}');
+  test.equal(actual(), expected, 'Replaces multiple instances of a string with another string');
+
+  expected = '1000000000 bucks';
+  actual = Handlebars.compile('{{replaceAll "30 bucks" 30 1000000000}}');
+  test.equal(actual(), expected, 'Replaces a number in a string');
+});
