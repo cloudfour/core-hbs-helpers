18db165396b81457a978b17c14cea47aecc782db
Tyler Sticka <tyler@tylersticka.com>
2019-07-29 14:44:03 -0700

    Add truthy tests to all and any

--- test/any.spec.js
+++ test/any.spec.js
@@ -11,19 +11,31 @@ tape('any', function (test) {
   var template;
   var actual;
 
-  test.plan(4);
+  test.plan(7);
 
   template = Handlebars.compile('{{#any a b c}}✔︎{{/any}}');
   actual = template({ a: true, b: true, c: true });
   test.equal(actual, expected, 'Resolves to true when all values are true');
 
+  template = Handlebars.compile('{{#any a b c d e f g}}✔︎{{/any}}');
+  actual = template({ a: true, b: 14, c: 'false', d: {}, e: [], f: '0', g: -42 });
+  test.equal(actual, expected, 'Resolves to true when all values are truthy');
+
   template = Handlebars.compile('{{#any a b c}}✔︎{{/any}}');
   actual = template({ a: false, b: true, c: false });
   test.equal(actual, expected, 'Resolves to true when any value is true');
 
+  template = Handlebars.compile('{{#any a b c}}✔︎{{/any}}');
+  actual = template({ a: false, b: 14, c: false });
+  test.equal(actual, expected, 'Resolves to true when any value is truthy');
+
   template = Handlebars.compile('{{#any a b c}}{{else}}✔︎{{/any}}');
   actual = template({ a: false, b: false, c: false });
   test.equal(actual, expected, 'Resolves to false when no value is true');
+
+  template = Handlebars.compile('{{#any a b c}}{{else}}✔︎{{/any}}');
+  actual = template({ a: false, b: undefined, c: false });
+  test.equal(actual, expected, 'Resolves to false when no value is truthy');
 
   template = Handlebars.compile('{{#if (any a b c)}}✔︎{{/if}}');
   actual = template({ a: false, b: true, c: false });
